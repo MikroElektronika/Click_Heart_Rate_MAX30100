14a72312ff1a5a6786ffac3d17046b3af2b6838a
ViktorMilovanovic <viktor.milovanovic@mikroe.com>
2016-05-31 14:50:04 +0200

    Fixed some comments

--- library/include/heartrate1_hw.h
+++ library/include/heartrate1_hw.h
@@ -72,6 +72,8 @@ void hr_temp_enable(bool enable);
  * Sets the operating state of the MAX30100
  * @param mode
  * Desired mode as stated in the datasheet (https://datasheets.maximintegrated.com/en/ds/MAX30100.pdf) page 15, table 3. "Mode Control"
+ * @return
+ * -1 if failed, 0 if successful
  */
 int8_t hr_set_mode(uint8_t mode);
 /**
@@ -86,6 +88,8 @@ void hr_spo2_high_res_enable(bool enable);
  * Sets the operating state of the MAX30100
  * @param sample_rate
  * Desired sample rate as stated in the datasheet (https://datasheets.maximintegrated.com/en/ds/MAX30100.pdf) page 16, table 4.
+ * @return
+ * -1 if failed, 0 if successful
  */
 int8_t hr_spo2_set_sample_rate(uint8_t sample_rate);
 /**
@@ -93,6 +97,8 @@ int8_t hr_spo2_set_sample_rate(uint8_t sample_rate);
  * Sets the LED pulse width (the IR and RED have the same pulse width)
  * @param _value
  * Desired pulse width as stated in the datasheet (https://datasheets.maximintegrated.com/en/ds/MAX30100.pdf) page 17, table 5.
+ * @return
+ * -1 if failed, 0 if successful
  */
 int8_t hr_set_led_pulse_width(uint8_t _value);
 /**
@@ -100,6 +106,8 @@ int8_t hr_set_led_pulse_width(uint8_t _value);
  * Sets the current level of the Red LED.
  * @param level
  * Desired current level as stated in the datasheet (https://datasheets.maximintegrated.com/en/ds/MAX30100.pdf) page 17, table 6.
+ * @return
+ * -1 if failed, 0 if successful
  */
 int8_t hr_set_red_current_level(uint8_t level);
 /**
@@ -107,6 +115,8 @@ int8_t hr_set_red_current_level(uint8_t level);
  * Sets the current level of the IR LED.
  * @param level
  * Desired current level as stated in the datasheet (https://datasheets.maximintegrated.com/en/ds/MAX30100.pdf) page 17, table 6.
+ * @return
+ * -1 if failed, 0 if successful
  */
 int8_t hr_set_ir_current_level(uint8_t level);
 /**
@@ -144,11 +154,15 @@ void hr_heartrate_spo2_data_ready_int_enable(bool enable);
  * Buffer to which to store the IR LED values
  * @param red_buffer
  * Buffer to which to store the RED LED values
+ * @return
+ * Returns the number of samples
  */
 uint8_t hr_read_diodes(uint16_t* ir_buffer, uint16_t* red_buffer);
 /**
  * @brief hr_get_status
  * Returns the value read from the interrupt status register. The value shows which interrupts have been trigerred
+ * @return
+ * Returns the status value
  */
 uint8_t hr_get_status();
 /**
